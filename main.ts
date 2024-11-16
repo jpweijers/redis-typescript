2b6b3c93b4e205c2faa85219deb5694f5bdccceb
Jean-Paul Weijers <jpweijers@users.noreply.github.com>
2024-11-16 10:28:51 +0100

    get and set value

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import { parseRESP } from "./resp-parser.ts";
 
 const PORT = 6379;
 
+const dataStore: Record<string, string> = {};
+
 const listener = Deno.listen({ port: PORT });
 
 for await (const conn of listener) {
@@ -23,6 +25,32 @@ async function handleConnection(conn: Deno.Conn) {
       await conn.write(
         new TextEncoder().encode(`$${args[0].length}\r\n${args[0]}\r\n`),
       );
+      break;
+    case "SET": {
+      const [arg1, arg2] = args;
+      if (!arg1 || !arg2) {
+        await conn.write(
+          new TextEncoder().encode("-ERR wrong number of arguments\r\n"),
+        );
+        break;
+      }
+      dataStore[arg1] = arg2;
+      await conn.write(new TextEncoder().encode("+OK\r\n"));
+      break;
+    }
+    case "GET":
+      {
+        const [arg1] = args;
+        if (!arg1) {
+          await conn.write(
+            new TextEncoder().encode("-ERR wrong number of arguments\r\n"),
+          );
+          break;
+        }
+        const value = dataStore[arg1];
+        const response = value ? new TextEncoder().encode(`$${value.length}\r\n${value}\r\n`) : new TextEncoder().encode("$-1\r\n");
+        await conn.write(response);
+      }
       break;
     default:
       await conn.write(new TextEncoder().encode("-ERR unknown command\r\n"));
